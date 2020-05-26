0704e4f71a5c821b765b3b989c17b56cc1f64c29
Michael Vezina <vezina95@gmail.com>
2020-05-26 13:44:08 -0400

    - Update some unit tests

--- src/test/java/epistemic/FailedConstructorPropositionTest.java
+++ src/test/java/epistemic/FailedConstructorPropositionTest.java
@@ -6,7 +6,9 @@ import jason.asSyntax.Atom;
 import jason.asSyntax.Literal;
 import jason.asSyntax.Term;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -16,16 +18,31 @@ import java.util.Collection;
 import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
-public class UnificationFailedPropositionTest extends PropositionTest {
+public class FailedConstructorPropositionTest extends PropositionTest {
 
+    @Rule
+    ExpectedException expectedException = ExpectedException.none();
 
-    public UnificationFailedPropositionTest(Literal key, Literal value) {
+    public FailedConstructorPropositionTest(Literal key, Literal value) {
         super(key, value);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void failToUnify()
     {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("The literalValue can not unify the literalKey. Failed to create Proposition.");
+
+        new Proposition(new WrappedLiteral(key), new WrappedLiteral(value));
+    }
+
+    @Test
+    @Parameterized.Parameter
+    public void valueNotGround()
+    {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("literalValue is not ground");
+
         new Proposition(new WrappedLiteral(key), new WrappedLiteral(value));
     }
     @Parameterized.Parameters(name = "Key: {0}, Value: {1}")
