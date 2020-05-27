5bc8d7619912858a1d1f3a97fa50855cc7baf161
Michael Vezina <vezina95@gmail.com>
2020-05-27 08:36:03 -0400

    - Test updates

--- src/test/java/epistemic/PropositionTest.java
+++ src/test/java/epistemic/PropositionTest.java
@@ -1,12 +1,10 @@
 package epistemic;
 
 import aggregators.PropAggregator;
-import aggregators.PropositionAggregator;
+import converters.WrappedLiteralArg;
 import epistemic.wrappers.WrappedLiteral;
-import jason.asSyntax.ASSyntax;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -17,20 +15,7 @@ import static jason.asSyntax.ASSyntax.createString;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class ValidPropositionTest {
-
-    public ValidPropositionTest() {
-    }
-
-    private static Stream<Arguments> validTestPropositionFixture() {
-        return Stream.of(
-                Arguments.of("hand('Alice', Card)", "hand('Alice', 'AA')"),
-                Arguments.of("ns::hand('Alice', Card)", "hand('Alice', 'AA')"),
-                Arguments.of("hand('Alice', Card)", "ns::hand('Alice', 'AA')"),
-                Arguments.of("ns::hand('Alice', Card)", "ns::hand('Alice', 'AA')")
-        );
-    }
-
+public class PropositionTest {
 
     @ParameterizedTest
     @MethodSource(value = "validTestPropositionFixture")
@@ -82,6 +67,45 @@ public class ValidPropositionTest {
     public void testHashCode(@PropAggregator Proposition currentProposition) {
         var clonedProposition = cloneProposition(currentProposition);
         assertEquals(clonedProposition.hashCode(), currentProposition.hashCode(), "clone hash should equal current prop hash");
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "failToUnifyFixture")
+    @DisplayName("Fail to unify key/value in proposition")
+    public void failToUnifyFixture(@WrappedLiteralArg WrappedLiteral key, @WrappedLiteralArg WrappedLiteral value) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Proposition(key, value));
+        assertEquals(exception.getMessage(), "The literalValue can not unify the literalKey. Failed to create Proposition.");
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "valueNotGroundFixture")
+    @DisplayName("Fail to create proposition, value must be ground.")
+    public void valueNotGroundFixture(@WrappedLiteralArg WrappedLiteral key, @WrappedLiteralArg WrappedLiteral value) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Proposition(key, value));
+        assertEquals(exception.getMessage(), "literalValue is not ground");
+
+    }
+    private static Stream<Arguments> validTestPropositionFixture() {
+        return Stream.of(
+                Arguments.of("hand('Alice', Card)", "hand('Alice', 'AA')"),
+                Arguments.of("ns::hand('Alice', Card)", "hand('Alice', 'AA')"),
+                Arguments.of("hand('Alice', Card)", "ns::hand('Alice', 'AA')"),
+                Arguments.of("ns::hand('Alice', Card)", "ns::hand('Alice', 'AA')")
+        );
+    }
+
+    private static Stream<Arguments> failToUnifyFixture() {
+        return Stream.of(
+                Arguments.of("test('Bob')", "test('Bob', 'Test')"),
+                Arguments.of("test('Alice', 'Test')", "test('Bob', 'Test')")
+        );
+    }
+
+    private static Stream<Arguments> valueNotGroundFixture() {
+        return Stream.of(
+                Arguments.of("test('Bob', Test)", "test('Bob', Test)"),
+                Arguments.of("test('Alice', Test)", "test('Bob', _)")
+        );
     }
 
     private Proposition cloneProposition(Proposition current) {
